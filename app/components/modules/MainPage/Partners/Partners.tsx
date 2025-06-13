28ff6b3535e8f3c72fac876f4a159c81687fc7ae
Baxa-1998 <baxtiyornarziev6@gmail.com>
2025-06-13 17:32:09 +0300

    img

--- app/components/modules/MainPage/Partners/Partners.tsx
+++ app/components/modules/MainPage/Partners/Partners.tsx
@@ -1,8 +1,11 @@
-'use client'
+'use client';
 import React from 'react';
 import styles from '@/app/styles/main-page/index.module.scss';
 import Image from 'next/image';
 import { useLang } from '@/app/hooks/useLang';
+import partners1 from '@/public/img/partners1.webp';
+import partners2 from '@/public/img/partners2.webp';
+import partners3 from '@/public/img/partners3.webp';
 export const Partners = () => {
   const { lang, translations } = useLang();
   return (
@@ -13,9 +16,9 @@ export const Partners = () => {
           <p className={styles.partners__subtitle}>{translations[lang].partners.subtitle}</p>
         </div>
         <div className={styles.partners__items}>
-          <Image width={245} height={64} src={'img/partners3.svg'} alt="partners" />
-          <Image width={326} height={48} src={'img/partners2.svg'} alt="partners" />
-          <Image width={240} height={64} src={'img/partners1.svg'} alt="partners" />
+          <Image width={245} height={64} src={partners3} alt="partners" />
+          <Image width={326} height={48} src={partners2} alt="partners" />
+          <Image width={240} height={64} src={partners1} alt="partners" />
         </div>
       </div>
     </div>

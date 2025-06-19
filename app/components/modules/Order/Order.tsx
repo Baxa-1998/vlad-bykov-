0d126521ea1d5beb519c576e4361b745fd2e9638
Baxa-1998 <baxtiyornarziev6@gmail.com>
2025-06-19 19:55:53 +0300

    commit

--- app/components/modules/Order/Order.tsx
+++ app/components/modules/Order/Order.tsx
@@ -4,14 +4,15 @@ import styles from '@/app/styles/order/styles.module.scss';
 import { $cart } from '@/app/context/cart';
 import { useUnit } from 'effector-react';
 import { Input } from '../../elements/Input';
-import { $location } from '@/app/context/country';
+import { $currencyRates, $location } from '@/app/context/country';
 import Image from 'next/image';
 import { ICartItem } from '@/app/types/cart';
 import { Button } from '../../elements/Button';
 import { useLang } from '@/app/hooks/useLang';
+import { convertPrice } from '@/app/lib/utils/convert-price';
 
 export const Order = () => {
-  const cart: ICartItem[] = useUnit($cart); 
+  const cart: ICartItem[] = useUnit($cart);
   const { translations, lang } = useLang();
 
   const location = useUnit($location);
@@ -56,7 +57,7 @@ export const Order = () => {
     address: '',
     apartment: '',
     country: location?.country_name || '',
-    city: '', 
+    city: '',
     zipCode: '',
     cardNumber: '',
     saveForNext: true,
@@ -67,6 +68,17 @@ export const Order = () => {
     deliveryMethod: '',
     paymentMethod: '',
   });
+
+  const rates = useUnit($currencyRates);
+
+  const { currencyCode, currencySymbol } = React.useMemo(() => {
+    return {
+      currencyCode: location?.currency.code || 'RUB',
+      currencySymbol: location?.currency.symbol || '₽',
+    };
+  }, [location]);
+
+  const convertedPrice = convertPrice(totalPrice ?? 0, rates, currencyCode);
 
   const handleSubmit = async () => {
     const res = await fetch('/api/send-order', {
@@ -128,125 +140,111 @@ export const Order = () => {
                 ))}
               </select>
             </div>
-        
           </div>
-              <div className={styles.userInfo}>
-              <Input
-                value={formData.name}
-                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                placeholder={'Имя'}
-                type={'text'}
-              />
-              <Input
-                value={formData.surname}
-                onChange={(e) => setFormData({ ...formData, surname: e.target.value })}
-                placeholder={'Фамилия'}
-                type={'text'}
-              />
+          <div className={styles.userInfo}>
+            <Input
+              value={formData.name}
+              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              placeholder={'Имя'}
+              type={'text'}
+            />
+            <Input
+              value={formData.surname}
+              onChange={(e) => setFormData({ ...formData, surname: e.target.value })}
+              placeholder={'Фамилия'}
+              type={'text'}
+            />
+          </div>
+          <div className={styles.company}>
+            <Input
+              value={formData.company}
+              onChange={(e) => setFormData({ ...formData, company: e.target.value })}
+              placeholder={'Компания (необязательно)'}
+              type={'text'}
+              required={false}
+            />
+          </div>
+          <div className={styles.adress}>
+            <Input
+              value={formData.address}
+              onChange={(e) => setFormData({ ...formData, address: e.target.value })}
+              placeholder={'Адресс'}
+              type={'text'}
+            />
+            <Image src={'/img/order-search.svg'} width={15} height={15} alt="search" />
+          </div>
+          <div className={styles.appartment}>
+            <Input
+              value={formData.apartment}
+              onChange={(e) => setFormData({ ...formData, apartment: e.target.value })}
+              placeholder={'Апартаменты'}
+              type={'text'}
+            />
+          </div>
+
+          <div className={styles.city}>
+            <Input
+              value={formData.zipCode}
+              onChange={(e) => setFormData({ ...formData, zipCode: e.target.value })}
+              placeholder={'Почтовый индекс'}
+              type={'text'}
+            />
+            <Input
+              value={formData.city}
+              onChange={(e) => setFormData({ ...formData, city: e.target.value })}
+              placeholder={'Город'}
+              type={'text'}
+            />
+          </div>
+
+          <div className={styles.saveOrder}>
+            <div>
+              <label className={styles.checkboxWrapper}>
+                <input
+                  checked={formData.saveForNext}
+                  onChange={(e) => setFormData({ ...formData, saveForNext: e.target.checked })}
+                  type="checkbox"
+                />
+                <span className={styles.checkmark}></span>
+              </label>
+
+              <h5>Сохранить для следующих заказов</h5>
             </div>
-            <div className={styles.company}>
-              <Input
-                value={formData.company}
-                onChange={(e) => setFormData({ ...formData, company: e.target.value })}
-                placeholder={'Компания (необязательно)'}
-                type={'text'}
-                required={false}
-              />
+            <div>
+              <label className={styles.checkboxWrapper}>
+                <input
+                  checked={formData.subscribeNews}
+                  onChange={(e) => setFormData({ ...formData, subscribeNews: e.target.checked })}
+                  type="checkbox"
+                />
+                <span className={styles.checkmark}></span>
+              </label>
+
+              <h5>Отправлять новости и акции</h5>
             </div>
-            <div className={styles.adress}>
-              <Input
-                value={formData.address}
-                onChange={(e) => setFormData({ ...formData, address: e.target.value })}
-                placeholder={'Адресс'}
-                type={'text'}
-              />
-              <Image src={'/img/order-search.svg'} width={15} height={15} alt="search" />
-            </div>
-            <div className={styles.appartment}>
-              <Input
-                value={formData.apartment}
-                onChange={(e) => setFormData({ ...formData, apartment: e.target.value })}
-                placeholder={'Апартаменты'}
-                type={'text'}
-              />
-            </div>
+          </div>
+          <div className={styles.methodDelivery}>
+            <h4 className={styles.orderTitle}>Метод доставки</h4>
 
-
-
-
-
-
-            <div className={styles.city}>
-                  <Input
-                value={formData.zipCode}
-                onChange={(e) => setFormData({ ...formData, zipCode: e.target.value })}
-                placeholder={'Почтовый индекс'}
-                type={'text'}
-              />
-              <Input
-                value={formData.city}
-                onChange={(e) => setFormData({ ...formData, city: e.target.value })}
-                placeholder={'Город'}
-                type={'text'}
-              />
-
-            </div>
-
-
-
-
-
-
-            <div className={styles.saveOrder}>
-              <div>
-                <label className={styles.checkboxWrapper}>
-                  <input
-                    checked={formData.saveForNext}
-                    onChange={(e) => setFormData({ ...formData, saveForNext: e.target.checked })}
-                    type="checkbox"
-                  />
-                  <span className={styles.checkmark}></span>
-                </label>
-
-                <h5>Сохранить для следующих заказов</h5>
-              </div>
-              <div>
-                <label className={styles.checkboxWrapper}>
-                  <input
-                    checked={formData.subscribeNews}
-                    onChange={(e) => setFormData({ ...formData, subscribeNews: e.target.checked })}
-                    type="checkbox"
-                  />
-                  <span className={styles.checkmark}></span>
-                </label>
-
-                <h5>Отправлять новости и акции</h5>
-              </div>
-            </div>
-            <div className={styles.methodDelivery}>
-              <h4 className={styles.orderTitle}>Метод доставки</h4>
-
-              {deliveryMethods.map((method) => (
-                <div key={method.label} className={styles.methodWrapper}>
-                  <div>
-                    <label className={styles.customCheckbox}>
-                      <input
-                        value={method.label}
-                        onChange={(e) =>
-                          setFormData({ ...formData, deliveryMethod: e.target.value })
-                        }
-                        type="radio"
-                        name="delivery"
-                      />
-                      <span className={styles.checkmark2}></span>
-                    </label>
-                    <h4>{method.label}</h4>
-                  </div>
-                  <h5>{method.price}</h5>
+            {deliveryMethods.map((method) => (
+              <div key={method.label} className={styles.methodWrapper}>
+                <div>
+                  <label className={styles.customCheckbox}>
+                    <input
+                      value={method.label}
+                      onChange={(e) => setFormData({ ...formData, deliveryMethod: e.target.value })}
+                      type="radio"
+                      name="delivery"
+                    />
+                    <span className={styles.checkmark2}></span>
+                  </label>
+                  <h4>{method.label}</h4>
                 </div>
-              ))}
+                <h5>{method.price}</h5>
+              </div>
+            ))}
 
-              {/* <div className={styles.methodWrapper}>
+            {/* <div className={styles.methodWrapper}>
                 <div>
                   <label className={styles.customCheckbox}>
                     <input type="radio" name="delivery" />
@@ -256,93 +254,88 @@ export const Order = () => {
                 </div>
                 <h5>700 ₽</h5>
               </div> */}
-            </div>
-            <div className={styles.payment}>
-              <h4 className={styles.orderTitle}>Оплата</h4>
+          </div>
+          <div className={styles.payment}>
+            <h4 className={styles.orderTitle}>Оплата</h4>
+            <Input
+              value={formData.cardNumber}
+              onChange={(e) => setFormData({ ...formData, cardNumber: e.target.value })}
+              placeholder={'Номер карты'}
+              type={'number'}
+            />
+            <div className={styles.date}>
               <Input
-                value={formData.cardNumber}
-                onChange={(e) => setFormData({ ...formData, cardNumber: e.target.value })}
-                placeholder={'Номер карты'}
+                value={formData.cardDate}
+                onChange={(e) => setFormData({ ...formData, cardDate: e.target.value })}
+                placeholder={'ММ/ГГ'}
                 type={'number'}
               />
-              <div className={styles.date}>
-                <Input
-                  value={formData.cardDate}
-                  onChange={(e) => setFormData({ ...formData, cardDate: e.target.value })}
-                  placeholder={'ММ/ГГ'}
-                  type={'number'}
-                />
-                <Input
-                  value={formData.cardCode}
-                  onChange={(e) => setFormData({ ...formData, cardCode: e.target.value })}
-                  placeholder={'Секретный код'}
-                  type={'number'}
-                />
-              </div>
               <Input
-                value={formData.cardHolder}
-                onChange={(e) => setFormData({ ...formData, cardHolder: e.target.value })}
-                placeholder={'Держатель карты'}
-                type={'text'}
+                value={formData.cardCode}
+                onChange={(e) => setFormData({ ...formData, cardCode: e.target.value })}
+                placeholder={'Секретный код'}
+                type={'number'}
               />
-              <div className={styles.carts}>
-                {paymentMethods.map((method) => (
-                  <div key={method.label} className={styles.methodWrapper}>
-                    <div>
-                      <label className={styles.customCheckbox}>
-                        <input value={method.label} onChange={(e) => setFormData({ ...formData, paymentMethod: e.target.value })} type="radio" name="payment" />
-                        <span className={styles.checkmark2}></span>
-                      </label>
-                      <h4>{method.label}</h4>
-                    </div>
-                    <Image src={method.img} width={38} height={34} alt={'payment'} />
-                  </div>
-                ))}
-
-        
-              
-                
-
-
-
-
-
-            
-
-
-
-
-
-
-              </div>
             </div>
+            <Input
+              value={formData.cardHolder}
+              onChange={(e) => setFormData({ ...formData, cardHolder: e.target.value })}
+              placeholder={'Держатель карты'}
+              type={'text'}
+            />
+            <div className={styles.carts}>
+              {paymentMethods.map((method) => (
+                <div key={method.label} className={styles.methodWrapper}>
+                  <div>
+                    <label className={styles.customCheckbox}>
+                      <input
+                        value={method.label}
+                        onChange={(e) =>
+                          setFormData({ ...formData, paymentMethod: e.target.value })
+                        }
+                        type="radio"
+                        name="payment"
+                      />
+                      <span className={styles.checkmark2}></span>
+                    </label>
+                    <h4>{method.label}</h4>
+                  </div>
+                  <Image src={method.img} width={38} height={34} alt={'payment'} />
+                </div>
+              ))}
+            </div>
+          </div>
         </form>
       </div>
 
-
-
-
-
       <div className={styles.orderSummary}>
         <div className={styles.orderSummaryWrapper}>
-          {cart?.map((item) => (
-            <div key={item.clientId} className={styles.orderSummaryItem}>
-              <div>
-                <Image src={item?.img} width={80} height={80} alt={'cart'} />
+          {cart?.map((item) => {
+            const convertedPrice = convertPrice(item.price ?? 0, rates, currencyCode);
+            return (
+              <div key={item.clientId} className={styles.orderSummaryItem}>
+                <div>
+                  <Image src={item?.img} width={80} height={80} alt={'cart'} />
 
-                <div className={styles.orderSummaryInfo}>
-                  <p className={styles.orderSummarySize}>{item.size}</p>
-                  <h2 className={styles.orderSummaryName}>{item.name}</h2>
+                  <div className={styles.orderSummaryInfo}>
+                    <p className={styles.orderSummarySize}>{item.size}</p>
+                    <h2 className={styles.orderSummaryName}>{item.name}</h2>
+                  </div>
+                </div>
+                <div>
+                  <span>
+                    {convertedPrice.toFixed(0)} {currencySymbol}
+                  </span>
                 </div>
               </div>
-              <div>
-                <span>{item.price} ₽</span>
-              </div>
-            </div>
-          ))}
+            );
+          })}
           <div className={styles.orderSummaryTotal}>
             <h5>{cart.length} товара</h5>
-            <p>{totalPrice}</p>
+            <p>
+              {' '}
+              {convertedPrice.toFixed(0)} {currencySymbol}
+            </p>
           </div>
           <div className={styles.orderSummaryDelivery}>
             <h5>Доставка</h5>
@@ -350,7 +343,9 @@ export const Order = () => {
           </div>
           <div className={styles.finalPrice}>
             <h5>Итого</h5>
-            <p>{totalPrice}</p>
+            <p>
+              {convertedPrice.toFixed(0)} {currencySymbol}
+            </p>
           </div>
           <Button onClick={handleSubmit} className={styles.orderSummaryBtn}>
             Оформить заказ

59eef08434cd2443798d1e89f39dbc3a3deb0a1e
Baxa-1998 <baxtiyornarziev6@gmail.com>
2025-06-25 22:01:53 +0300

    rewrite

--- app/constants/corsHeaders.ts
+++ app/constants/corsHeaders.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://vlad-bykov-admin.vercel.app',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS, PATCH',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },

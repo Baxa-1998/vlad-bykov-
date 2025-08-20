8c41edbf40b497280f434a0c0433258600ccca26
Baxa-1998 <baxtiyornarziev6@gmail.com>
2025-08-20 18:22:41 +0300

    about

--- app/components/modules/MainPage/HistoryBrand/HistoryBrand.tsx
+++ app/components/modules/MainPage/HistoryBrand/HistoryBrand.tsx
@@ -5,6 +5,7 @@ import styles from '@/app/styles/main-page/index.module.scss';
 import { Button } from '@/app/components/elements/Button';
 import { Title } from '@/app/components/elements/Title';
 import { useLang } from '@/app/hooks/useLang';
+import Link from 'next/link';
 
 export const HistoryBrand = () => {
   const { translations, lang } = useLang();
@@ -14,7 +15,11 @@ export const HistoryBrand = () => {
         <Title className={styles.historyBrandTitle}>{translations[lang].historyBrand.title}</Title>
         {/* <h1 className={styles.historyBrandTitle}></h1> */}
         <p className={styles.historyBrandSubtitle}>{translations[lang].historyBrand.subtitle} </p>
-        <Button className={styles.historyBrandBtn}>{translations[lang].historyBrand.button}</Button>
+        <Link href={'/about'}>
+          <Button className={styles.historyBrandBtn}>
+            {translations[lang].historyBrand.button}
+          </Button>
+        </Link>
       </div>
     </div>
   );
